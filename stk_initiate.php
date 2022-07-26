2a1ab1db96d1ececf2977f5a8e65d8aea647da9b
sly-designs <55442037+sly-designs@users.noreply.github.com>
2022-07-26 16:25:14 +0300

    second commit

--- stk_initiate.php
+++ stk_initiate.php
@@ -5,8 +5,8 @@ if(isset($_POST['submit'])){
   date_default_timezone_set('Africa/Nairobi');
 
   # access token
-  $consumerKey = 'Keypt7N48B9aSa7P05c3XEY9tTrMO3nw'; //Fill with your app Consumer Key
-  $consumerSecret = 'SQhtNXAk1U2ni28V'; // Fill with your app Secret
+  $consumerKey = 'ULUn6LyvQkD8KGvVdARnuAZuiF1gtyHC'; //Fill with your app Consumer Key
+  $consumerSecret = 'IylfekTcZaArRnGa'; // Fill with your app Secret
 
   # define the variales
   # provide the following details, this part is found on your test credentials on the developer account
@@ -42,7 +42,7 @@ if(isset($_POST['submit'])){
   $initiate_url = 'https://sandbox.safaricom.co.ke/mpesa/stkpush/v1/processrequest';
 
   # callback url
-  $CallBackURL = 'https://morning-forest-72309.herokuapp.com/callback_url.php';  
+  $CallBackURL = 'https://tranquil-forest-86462.herokuapp.com/callback_url.php';  
 
   $curl = curl_init($access_token_url);
   curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
@@ -65,17 +65,17 @@ if(isset($_POST['submit'])){
 
   $curl_post_data = array(
     //Fill in the request parameters with valid values
-    'BusinessShortCode' => $BusinessShortCode,
-    'Password' => $Password,
-    'Timestamp' => $Timestamp,
-    'TransactionType' => 'CustomerPayBillOnline',
+    'BusinessShortCode' => 174379,
+    'Password' =>" MTc0Mzc5YmZiMjc5ZjlhYTliZGJjZjE1OGU5N2RkNzFhNDY3Y2QyZTBjODkzMDU5YjEwZjc4ZTZiNzJhZGExZWQyYzkxOTIwMjIwNzI2MTU0MzE5",
+    'Timestamp' => "20220726154319",
+    'TransactionType' => "CustomerPayBillOnline",
     'Amount' => $Amount,
-    'PartyA' => $PartyA,
-    'PartyB' => $BusinessShortCode,
-    'PhoneNumber' => $PartyA,
-    'CallBackURL' => $CallBackURL,
-    'AccountReference' => $AccountReference,
-    'TransactionDesc' => $TransactionDesc
+    'PartyA' => 254113868410,
+    'PartyB' => 174379,
+    'PhoneNumber' => 254113868410,
+    'CallBackURL' => https://mydomain.com/path,
+    'AccountReference' => "CompanyXLTD",
+    'TransactionDesc' => "Payment of X"
   );
 
   $data_string = json_encode($curl_post_data);
